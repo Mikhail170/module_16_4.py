5fe222089b2f420ec94513e2ccee557abe43ba20
Mikhail170 <ga_pe@mail.ru>
2024-10-15 16:49:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,28 @@ async def get_users() -> List[User]:
     return users
 
 @app.post('/user/', response_model=User)
-def create_user(user: User) -> User:
-    if any(u.id == user.id for u in users):
-        raise HTTPException(status_code=400, detail="User with this ID already exists")
-    users.append(user)
-    return user
+async def post_user(username: str, age: int) -> User:
+    new_id = 1 if not users else users[-1].id + 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
 
 @app.put('/user/{user_id}', response_model=User)
-async def update_user(user_id: int, updated_user: User) -> User:
-    for index, user in enumerate(users):
+async def update_user(user_id: int, username: str, age: int) -> User:
+    for user in users:
         if user.id == user_id:
-            users[index] = updated_user
-            return updated_user
-    raise HTTPException(status_code=404, detail='User not found')
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
 
 @app.delete('/user/{user_id}')
-def delete_user(user_id: int) -> str:
+async def delete_user(user_id: int) -> User:
     for index, user in enumerate(users):
         if user.id == user_id:
-            users.pop(index)
-            return f'User {user_id} has been deleted'
-    raise HTTPException(status_code=404, detail='User not found')
-
+            deleted_user = users.pop(index)
+            return deleted_user
+    raise HTTPException(status_code=404, detail="User was not found")
 
 
 
